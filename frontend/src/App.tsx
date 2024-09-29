e41b70884d4e780836677bef9e4f6bd8ac1a4791
koog7 <k.ognev709@mail.com>
2024-09-29 15:04:37 +0600

    Инициализирован React+TS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+
+    return(
+        <>
+            <canvas style={{border:'1px solid black'}} width={400} height={400}></canvas>
+        </>
+    )
+
+};
+
+export default App
