93cc9015899ad024614bbde29374f62f9eafef13
koog7 <k.ognev709@mail.com>
2024-09-30 18:35:58 +0600

    Добавлена возможность рисовать на холсте

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from "./container/Home.tsx";
 
 const App = () => {
 
     return(
         <>
-            <canvas style={{border:'1px solid black'}} width={400} height={400}></canvas>
+            <Home />
         </>
     )
 
---
+++ frontend/src/container/Home.tsx
@@ -0,0 +1,78 @@
+import {useEffect, useRef, useState} from 'react';
+import * as React from "react";
+
+const Home = () => {
+    interface PosProps{
+        x: number;
+        y: number;
+        lastX: number;
+        lastY: number;
+    }
+
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [isActive, setIsActive] = useState(false);
+    const [lastPosition, setLastPosition] = useState({x: 0, y: 0});
+    const [pixels, setPixels] = useState<PosProps[]>([]);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+
+        if (!canvas) {
+            return null;
+        }
+
+        const canvasContext = canvas?.getContext('2d');
+
+        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+        pixels.forEach(({ x, y, lastX, lastY }) => {
+            canvasContext.beginPath();
+            canvasContext.moveTo(lastX, lastY);
+            canvasContext.lineTo(x, y);
+            canvasContext.stroke();
+        });
+    }, [pixels]);
+
+    const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        const {offsetX, offsetY, type} = e.nativeEvent;
+
+        if (type === 'mousedown') {
+            setIsActive(true);
+            setLastPosition({x: offsetX, y: offsetY});
+        }
+
+        if (type === 'mousemove' && isActive) {
+            const pixelPosData = {
+                x: offsetX,
+                y: offsetY,
+                lastX: lastPosition.x,
+                lastY: lastPosition.y,
+            };
+
+            setPixels((pixel) => [...pixel, pixelPosData]);
+            setLastPosition({ x: offsetX, y: offsetY });
+        }
+
+        if (type === 'mouseup') {
+            setIsActive(false);
+        }
+    };
+
+    return (
+
+        <div style={{display:'flex', flexDirection:'column', marginTop:'50px'}}>
+            <canvas
+                ref={canvasRef}
+                width={800}
+                height={600}
+                style={{border: '1px solid black', backgroundColor: 'white'}}
+                onMouseDown={draw}
+                onMouseMove={draw}
+                onMouseUp={draw}
+                onMouseLeave={draw}
+            />
+        </div>
+    );
+};
+
+export default Home;
